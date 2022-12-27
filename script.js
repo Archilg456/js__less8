805500ba86e50d813f2286e17b67c3be7c7fbd9f
Archilg456 <92914320+Archilg456@users.noreply.github.com>
2022-12-27 13:55:04 +0400

    finish

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 "use strict";
+ 
+// დავალება 1 
 
 
+ fetch("https://reqres.in/api/unknown",{
+    method: "GET",
+ })
+
+.then(function(recivedInfo){
+    if(recivedInfo.status !== 200){
+         throw recivedInfo.status;
+    }
+    return recivedInfo.json();
+ })
+
+.then(function(recivedInfoJS) {
+    
+    let ul = document.createElement('ul');
+    recivedInfoJS.data.forEach((eLement) => {
+    let li = document.createElement('li');
+    li.textContent = `${eLement.name}`; 
+    let li__color = eLement.color;
+    li.style.color = li__color;
+    ul.appendChild(li);
+    })
+
+document.getElementById('user__info').appendChild(ul);
+console.log(recivedInfoJs);
+})
+.catch(function(Error){
+    if (Error == 404) {
+        
+        let p = document.createElement("p");
+        p.textContent = 'Server Error' ;
+
+        document.getElementById("user__info").appendChild(p);
+      }
+})
 
 // დავალება 2
+
 
 function getUsers(){
 
@@ -17,6 +54,7 @@ function getUsers(){
         recivedInfoJs.data.forEach(eLement => {
             let li = document.createElement('li');
             let img = document.createElement('img');
+            img.style.marginTop = "12px";
             li.textContent = `${eLement.first_name} ${eLement.last_name}`;
             img.src = `${eLement.avatar}`;
             li.appendChild(img);
@@ -35,6 +73,9 @@ function getUsers(){
     
     request.open('GET', 'https://reqres.in/api/users?page=2');
     request.send();
-
 }
 getUsers();
+
+
+
+
